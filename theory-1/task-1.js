ac81955b9b46dfe44821595be928f3ca22cc4c46
Maksim <max-goryachev@ya.ru>
2023-09-20 21:20:33 +0600

    create a practice

--- theory-1/task-1.js
+++ theory-1/task-1.js
@@ -1,27 +1,27 @@
 //task
-const arr = [10, 12, 15, 21];
-// for (var i=0 ; i<arr.length ; i++){
-//     setTimeout(function(){
-//         console.log(arr[i]>13 ? `Good:${arr[i]}` : `Bad:${arr[i]}`)
-//     } ,3000)
-// }
+// const arr = [10, 12, 15, 21];
+// // for (var i=0 ; i<arr.length ; i++){
+// //     setTimeout(function(){
+// //         console.log(arr[i]>13 ? `Good:${arr[i]}` : `Bad:${arr[i]}`)
+// //     } ,3000)
+// // }
 
-// map
-let a = []
+// // map
+// let a = []
 
-arr.map((num) => {
+// arr.map((num) => {
     
-    num > 13 ? a.push(`Good: ${num}`) : a.push(`Bad: ${num}`)
+//     num > 13 ? a.push(`Good: ${num}`) : a.push(`Bad: ${num}`)
     
-})
-b= a.join(', ')
-// setTimeout(()=>console.log(b),3000);
+// })
+// b= a.join(', ')
+// // setTimeout(()=>console.log(b),3000);
 
-//forEach
-let c=[]
+// //forEach
+// let c=[]
 
-arr.forEach(num=>{
-    num > 13 ? c.push(`Good: ${num}`) : c.push(`Bad: ${num}`)
-})
-d= c.join(', ')
-setTimeout(()=>console.log(d),3000);
+// arr.forEach(num=>{
+//     num > 13 ? c.push(`Good: ${num}`) : c.push(`Bad: ${num}`)
+// })
+// d= c.join(', ')
+// // setTimeout(()=>console.log(d),3000);
