392a461487d3c517faf8ded7cca8d104c301011f
Maksim <max-goryachev@ya.ru>
2023-09-21 20:20:12 +0600

    finished js code

--- practice-1/index.js
+++ practice-1/index.js
@@ -1,14 +1,15 @@
 const userName = document.querySelector('.name'),
     phone = document.querySelector('.phone'),
-    email = document.querySelector('.email'),
-    select = document.querySelector('.select'),
+    email = document.getElementById('email'),
+    select = document.getElementById('select'),
+    options = document.querySelector('.options'),
     startBtn = document.querySelector('.start-button'),
     acceptBtn = document.querySelector('.button-block__accept'),
     cancelBtn = document.querySelector('.button-block__cancel'),
     modal = document.querySelector('.container'),
     mainContainer = document.querySelector('.main-container');
 
-//Модальное окно    
+//Модальное окно   
 
 startBtn.addEventListener('click', () => {
     modal.style.display = 'block'
@@ -28,8 +29,20 @@ mainContainer.addEventListener('click', (e) => {
 
 //Всплывающее меню select
 
+select.addEventListener('click', (e) => {
+    options.classList.toggle('active')
+    options.addEventListener('click', (e) => {
+        select.textContent = e.target.textContent
+        options.classList.remove('active')
+    })
+}
+)
 
-
+modal.addEventListener('click', (e) => {
+    if (e.target.classList.value !== 'header-main__block__input select required' && e.target.classList.value !== 'options' && e.target.classList.value !== 'options__option') {
+        options.classList.remove('active')
+    }
+})
 
 //Кнопка Стать партнером проекта проверяет заполненность полей
 
@@ -38,29 +51,75 @@ function requiredHandler() {
     let nameValid = false,
         phoneValid = false,
         emailValid = false,
+        selectValid = false,
         formValid;
-
+    //Проверки нейма-----
     if (userName.value === '') {
         userName.style.border = '1px solid red';
+        nameValid = false
+    } else {
+        userName.style.border = '1px solid #ccc'
+        nameValid = true
+    }
+    if (userName.value.length < 3) {
+        userName.style.border = '1px solid red'
+        nameValid = false
 
     } else {
         userName.style.border = '1px solid #ccc'
         nameValid = true
     }
+    //----------------------
+
+    //Проверки телефона-----
     if (phone.value === '') {
         phone.style.border = '1px solid red'
+        phoneValid = false
     } else {
         phone.style.border = '1px solid #ccc'
         phoneValid = true
     }
+    if (phone.value.length < 3) {
+        phone.style.border = '1px solid red'
+        phoneValid = false
+
+    } else {
+        phone.style.border = '1px solid #ccc'
+        phoneValid = true
+    }
+    //-----------------------
+
+    //Проверки емайла---
     if (email.value === '') {
         email.style.border = '1px solid red'
+        emailValid = false
     } else {
         email.style.border = '1px solid #ccc'
         emailValid = true
-
     }
-    formValid = [nameValid, phoneValid, emailValid];
+    if (email.value.length < 3) {
+        email.style.border = '1px solid red'
+        emailValid = false
+    } else {
+        email.style.border = '1px solid #ccc'
+        emailValid = true
+    }
+    if (email.value.indexOf('@') === -1) {
+        email.style.border = '1px solid red'
+        emailValid = false
+    } else {
+        email.style.border = '1px solid #ccc'
+        emailValid = true
+    }
+    //------------------
+
+    if (select.textContent === '') {
+        select.style.border = '1px solid red'
+    } else {
+        select.style.border = '1px solid #ccc'
+        selectValid = true
+    }
+    formValid = [nameValid, phoneValid, emailValid, selectValid];
     const acceptForm = formValid.indexOf(false)
     if (acceptForm === -1) {
         acceptBtn.style.color = 'black '
@@ -70,4 +129,21 @@ function requiredHandler() {
         alert('Заполнены не все поля!')
     }
 }
+
+//Ограничиваем ввод букв
+
+phone.addEventListener('keydown', function (event) {
+    // Разрешаем: backspace, delete, tab и escape
+    if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+        // Разрешаем: home, end, влево, вправо
+        (event.keyCode >= 35 && event.keyCode <= 39)) {
+        // Ничего не делаем
+        return;
+    } else {
+        // Запрещаем все, кроме цифр на основной клавиатуре, а так же Num-клавиатуре
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+            event.preventDefault();
+        }
+    }
+});
 
