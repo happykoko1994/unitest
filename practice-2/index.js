5d86c7219fd1b22aa5e4da17941e62f01f4c13b0
Maksim <max-goryachev@ya.ru>
2023-09-22 10:21:24 +0600

    fiinished js

---
+++ practice-2/index.js
@@ -0,0 +1,21 @@
+const tbody = document.querySelector('tbody')
+function request() {
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => {
+            for (i = 0; i < data.length; i++) {
+                let newRow = document.createElement('tr')
+                newRow.innerHTML = `<td>${data[i].id}</td><td>${data[i].userId}</td><td>${data[i].title}</td><td>${data[i].body}</td>`
+                tbody.append(newRow)
+                }
+                
+
+    console.log(data);
+
+
+});
+}
+request()
+
+
