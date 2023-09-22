6814b5cce22a463e703e08751a6a70b690a62674
Maksim <max-goryachev@ya.ru>
2023-09-22 14:56:59 +0600

    add sort

--- practice-3/index.js
+++ practice-3/index.js
@@ -1,6 +1,10 @@
-const tbody = document.querySelector('tbody')
-function request() {
+const table = document.querySelector("#sortableTable");
+const th = table.querySelectorAll("th");
+let tbody = table.querySelector("tbody");
 
+// Получение данных
+
+function request() {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then((response) => response.json())
         .then((data) => {
@@ -8,14 +12,50 @@ function request() {
                 let newRow = document.createElement('tr')
                 newRow.innerHTML = `<td>${data[i].id}</td><td>${data[i].userId}</td><td>${data[i].title}</td><td>${data[i].body}</td>`
                 tbody.append(newRow)
-                }
-                
-
-    console.log(data);
-
-
-});
+            }
+        });
 }
+
+
+// Сортировка таблица
+
+th.forEach((header) => {
+    header.addEventListener("click", function () {
+        let rows = [...tbody.rows];
+        let columnIndex = header.cellIndex;
+        let sortDirection = header.getAttribute("data-sort-direction") === "asc" ? "desc" : "asc";
+        header.setAttribute("data-sort-direction", sortDirection);
+        rows.sort((a, b) => {
+
+            if (columnIndex > 2) {
+                let aValue = a.cells[columnIndex].textContent;
+                let bValue = b.cells[columnIndex].textContent;
+
+                if (sortDirection === "asc") {
+                    return aValue > bValue ? 1 : -1;
+                } else {
+                    return bValue > aValue ? 1 : -1;
+                }
+            } else {
+                let aValue = +a.cells[columnIndex].textContent;
+                let bValue = +b.cells[columnIndex].textContent;
+
+                if (sortDirection === "asc") {
+                    return aValue > bValue ? 1 : -1;
+                } else {
+                    return bValue > aValue ? 1 : -1;
+                }
+            }
+        });
+
+        tbody.remove();
+        tbody = document.createElement("tbody");
+        rows.forEach((row) => tbody.appendChild(row));
+        table.appendChild(tbody);
+    });
+});
 request()
+window.addEventListener('click', (e) => {
+    console.log(typeof e.target.textContent);
 
-
+})
